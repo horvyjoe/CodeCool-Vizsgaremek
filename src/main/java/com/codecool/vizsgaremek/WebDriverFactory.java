e18371d2aca4272a1f448c442b7a0649593a2469
horvy <horvyjoe@gmail.com>
2023-04-18 17:11:40 +0200

    Pom.xml fix update WebDriverFactory

--- src/main/java/com/codecool/vizsgaremek/WebDriverFactory.java
+++ src/main/java/com/codecool/vizsgaremek/WebDriverFactory.java
@@ -11,13 +11,20 @@ public class WebDriverFactory {
 
     public static WebDriver getWebDriver() {
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("window-size=1280,800");
+        new ChromeOptions();
+        options.addArguments("--no-sandbox",
+                "--disable-dev-shm-usage",
+                "--disable-extensions",
+                "incognito",
+                "window-size=1280,800",
+                "--headless");
+        /*options.addArguments("window-size=1280,800");
         options.addArguments("incognito");
         options.addArguments("disable-extensions");
         options.addArguments("remote-allow-origins=*");
         options.addArguments("disable-dev-shm-usage");
         options.addArguments("no-sandbox");
-        options.addArguments("headless");
+        options.addArguments("headless");*/
         //WebDriver driver = new ChromeDriver(options);
         //return driver;
         return new ChromeDriver(options);
